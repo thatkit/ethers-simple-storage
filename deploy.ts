27829db5f8f28ab814bca4877da008b45199a719
Nikita <emper137137@gmail.com>
2025-05-24 22:34:40 +1000

    hardhat: add for local rpc node

--- deploy.ts
+++ deploy.ts
@@ -1,4 +1,25 @@
-const main = async () => {};
+import { ContractFactory, JsonRpcProvider, Wallet } from 'ethers';
+import { strict } from 'assert';
+import { readFile } from 'fs/promises';
+import 'dotenv/config';
+
+const provider = new JsonRpcProvider('http://127.0.0.1:8545/');
+
+const main = async () => {
+  strict(process.env.PRIVATE_KEY, 'must set PRIVATE_KEY');
+
+  const deployer = new Wallet(process.env.PRIVATE_KEY, provider);
+
+  const [abi, binary] = await Promise.all([
+    readFile('./SimpleStorage_sol_SimpleStorage.abi', 'utf8'),
+    readFile('./SimpleStorage_sol_SimpleStorage.bin', 'utf8'),
+  ]);
+
+  const contractFactory = new ContractFactory(abi, binary, deployer);
+
+  const contract = await contractFactory.deploy();
+  console.log(contract);
+};
 
 main()
   .then(() => {
