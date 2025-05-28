791ff4dfeaefa1ab9048992c7d4cef111884c55a
Nikita <emper137137@gmail.com>
2025-05-28 21:04:11 +1000

    add contract calls

--- deploy.ts
+++ deploy.ts
@@ -2,6 +2,7 @@ import { ContractFactory, JsonRpcProvider, Wallet } from 'ethers';
 import { strict } from 'assert';
 import { readFile } from 'fs/promises';
 import 'dotenv/config';
+import abi from './SimpleStorage_sol_SimpleStorage.json';
 
 const provider = new JsonRpcProvider('http://127.0.0.1:8545/');
 
@@ -10,15 +11,70 @@ const main = async () => {
 
   const deployer = new Wallet(process.env.PRIVATE_KEY, provider);
 
-  const [abi, binary] = await Promise.all([
-    readFile('./SimpleStorage_sol_SimpleStorage.abi', 'utf8'),
-    readFile('./SimpleStorage_sol_SimpleStorage.bin', 'utf8'),
-  ]);
+  const binary = await readFile(
+    './SimpleStorage_sol_SimpleStorage.bin',
+    'utf8'
+  );
 
-  const contractFactory = new ContractFactory(abi, binary, deployer);
+  /**
+   * Deploying using contract factory
+   */
+
+  const contractFactory = new ContractFactory<typeof abi>(
+    abi,
+    binary,
+    deployer
+  );
 
   const contract = await contractFactory.deploy();
-  console.log(contract);
+  console.log('contract', contract);
+
+  // wait for 1 block to confirm
+  const deploymentReceipt = await contract.deploymentTransaction()?.wait(1);
+  console.log('deploymentReceipt', deploymentReceipt);
+
+  // /**
+  //  * Deploying using only the tx data
+  //  */
+
+  // const [nonce, network] = await Promise.all([
+  //   deployer.getNonce(),
+  //   provider.getNetwork(),
+  // ]);
+
+  // const txParams = {
+  //   nonce,
+  //   gasPrice: 1522031607n,
+  //   gasLimit: 1e6,
+  //   to: null,
+  //   value: 0,
+  //   data: `0x${binary}`,
+  //   chainId: network.chainId,
+  // };
+
+  // console.log('TX params:', txParams);
+
+  // // also signing behind the scene
+  // const txResponse = await deployer.sendTransaction(txParams);
+  // console.log('TX response:', txResponse);
+
+  // const txReceipt = await txResponse.wait();
+  // console.log('TX receipt:', txReceipt);
+
+  /**
+   * Interacting with the contract
+   *
+   * p.s. for some reason, autocomplete by TypeScript is not working
+   */
+
+  const currentFavoriteNumber = await (contract as any).retrieve();
+  console.log('currentFavoriteNumber', currentFavoriteNumber);
+
+  const storeTxResponse = await (contract as any).store(50n);
+  const storeTxReceipt = await storeTxResponse.wait(1);
+
+  const newFavoriteNumber = await (contract as any).retrieve();
+  console.log('newFavoriteNumber', newFavoriteNumber);
 };
 
 main()
